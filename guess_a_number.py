df5d19fcad79b3ffaf1a987746d34def225c1a84
Stely <stelyshtonova@gmail.com>
2024-01-22 00:30:51 +0200

    Added colors to the terminal, checks for invalid responses, set attempt max to three, checks if the attempts are exceeded, made two possible outputs- if the attempts are exceeded or if the user guessed the number correctly, prompts the user to play again

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,4 +1,5 @@
 from random import randint
+
 # colors
 Red = '\033[31m'
 Green = '\033[32m'
@@ -9,21 +10,38 @@ Magenta = '\033[35m'
 
 # the computer chooses a random number between 1 and 100
 computers_number = randint(1, 100)
+attempts_count = 0
+have_guessed = False
 while True:
     # getting user's input
     your_guess = input(Green + 'Guess the number chosen by the computer (1 - 100):\n')
     # checking if the user's guess is an integer and if it's within the requirements
     if not your_guess.isdigit():
-        print('Invalid input! Please try again...')
+        print(Red + 'Invalid input! Please try again...')
         continue
     elif int(your_guess) > 100 or int(your_guess) < 1:
-        print('Invalid input! Please try again...')
+        print(Red + 'Invalid input! Please try again...')
         continue
+    else:
+        attempts_count += 1
+        # checking if the attempts are over the limit
+    if attempts_count > 3:
+        print(Purple + 'You lost! You have no more tries left... Would you like to play again?')
+        answer = input(Orange + 'Answer with [yes] or [no]: ')
+        if answer.lower() == 'yes':
+            attempts_count = 0
+            continue
+        elif answer.lower() == 'no':
+            print(Blue + 'Thanks for playing...')
+            raise SystemExit
+        else:
+            print(Red + 'Invalid input!')
     # comparing numbers
     if int(your_guess) == computers_number:
-        print(Magenta + 'Congratulations! The computer chose the number ' \
-              + Red + str(computers_number) + Magenta + ' and you guessed it!')
-        break
+        print(Magenta + 'Congratulations! The computer chose the number ' + \
+            Red + str(computers_number) + Magenta + f' and you guessed it on the' + \
+            Red + str(attempts_count) + Magenta + 'try!')
+        have_guessed = True
     else:
         print(Blue + 'Not quite...')
         # dropping hints
@@ -32,4 +50,18 @@ while True:
             continue
         elif int(your_guess) < computers_number:
             print(Purple + 'Try with a bigger number...')
+            continue
+    while have_guessed:
+        # prompting the user to play again
+        print(Blue + 'Would you like to play again?')
+        answer = input(Blue + 'Answer with [yes] or [no]: ')
+        if answer.lower() == 'yes':
+            attempts_count = 0
+            have_guessed = False
+            break
+        elif answer.lower() == 'no':
+            print(Blue + 'Thanks for playing...')
+            raise SystemExit
+        else:
+            print(Red + 'Invalid input!')
             continue
