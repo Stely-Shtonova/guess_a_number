6b51d467141356d8e4947f740da6e8296ed43381
Stely <stelyshtonova@gmail.com>
2024-01-24 10:26:34 +0200

    Added 3 levels to the game

--- guess_a_number.py
+++ guess_a_number.py
@@ -9,26 +9,33 @@ Purple = '\033[35m'
 Magenta = '\033[35m'
 
 # the computer chooses a random number between 1 and 100
-computers_number = randint(1, 100)
+max_parameter = 50
+computers_number = randint(1, max_parameter)
 attempts_count = 0
+levels_count = 1
 have_guessed = False
 while True:
     # getting user's input
-    your_guess = input(Green + 'Guess the number chosen by the computer (1 - 100):\n')
+    your_guess = input(Red + f'Level {levels_count}! ' + Green + f'Guess the number chosen by the computer (1 - ' + Red \
+                       + f'{max_parameter}' + Green + '):\n')
     # checking if the user's guess is an integer and if it's within the requirements
     if not your_guess.isdigit():
         print(Red + 'Invalid input! Please try again...')
         continue
-    elif int(your_guess) > 100 or int(your_guess) < 1:
+    elif int(your_guess) not in range(1, max_parameter + 1):
         print(Red + 'Invalid input! Please try again...')
         continue
     else:
         attempts_count += 1
     # comparing numbers
     if int(your_guess) == computers_number:
+        if levels_count == 3:
+            print(Orange + 'YOU WON!!!')
+            raise SystemExit
         print(Magenta + 'Congratulations! The computer chose the number ' + \
-            Red + str(computers_number) + Magenta + f' and you guessed it on the ' + \
-            Red + str(attempts_count) + Magenta + ' try!')
+              Red + str(computers_number) + Magenta + f' and you guessed it on the ' + \
+              Red + str(attempts_count) + Magenta + ' try!')
+        levels_count += 1
         have_guessed = True
     else:
         print(Blue + 'Not quite...')
@@ -39,12 +46,17 @@ while True:
             print(Purple + 'Try with a bigger number...')
     while have_guessed:
         # prompting the user to play again
-        print(Blue + 'Would you like to play again?')
+        print(Blue + f'Would you like to go to... ' + Red + f'Level {levels_count}?')
         answer = input(Blue + 'Answer with [yes] or [no]: ')
         if answer.lower() == 'yes':
             attempts_count = 0
-            computers_number = randint(1, 100)
             have_guessed = False
+            if levels_count == 2:
+                max_parameter = 75
+                computers_number = randint(1, max_parameter)
+            elif levels_count == 3:
+                max_parameter = 100
+                computers_number = randint(1, max_parameter)
             break
         elif answer.lower() == 'no':
             print(Blue + 'Thanks for playing...')
@@ -55,11 +67,15 @@ while True:
     # checking if the attempts are over the limit
     while attempts_count == 3:
         # prompting the user to play again
-        print(Purple + f'You lost- you have no more tries left. The number was {computers_number}. Would you like to play again?')
+        print(
+            Purple + f'You lost- you have no more tries left. The number was {computers_number}. Would you like to '
+                     f'play again?')
         answer = input(Orange + 'Answer with [yes] or [no]: ')
         if answer.lower() == 'yes':
             attempts_count = 0
-            computers_number = randint(1, 100)
+            levels_count = 1
+            max_parameter = 50
+            computers_number = randint(1, max_parameter)
             break
         elif answer.lower() == 'no':
             print(Blue + 'Thanks for playing...')
